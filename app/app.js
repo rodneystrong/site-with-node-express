f058db929fa3d9e17218c281b391e643f511c953
aleeex <akang2@gmail.com>
2016-10-07 22:10:32 -0700

    added a console log once server is run + changed content type to support html

--- app/app.js
+++ app/app.js
@@ -2,15 +2,14 @@ var http = require('http');
 
 //req and res stand for request and response
 var myServer = http.createServer(function(req,res) {
-  res.writeHead(200, {'content-type' : 'text/plain'});
+  res.writeHead(200, {'content-type' : 'text/html'});
 
   //now we send a response back
-  res.write('nice meetups');
+  res.write('<h1>nice meetups</h1>');
   //the next line lets us know the response is done
   res.end();
-
-
 
 });
 
 myServer.listen(3000);
+console.log('Go to localhost:3000 on your browser');
