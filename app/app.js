4cfc9d9668fdf27e15cd335f0744fede35f51b8d
aleeex <akang2@gmail.com>
2016-10-08 09:39:05 -0700

    comment to understand status code in res to user

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 //req and res stand for request and response
 var myServer = http.createServer(function(req,res) {
+  //the '200' is the status code we send back as a 'res' or response
+  //to the user's header (hence the writeHead method)
   res.writeHead(200, {'content-type' : 'text/html'});
 
   //now we send a response back
