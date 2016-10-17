10cc1a7e0821738529483a3f4b648bf2317b4b67
aleeex <akang2@gmail.com>
2016-10-16 22:18:47 -0700

    changed app var to router due to using router now and exported router using module.exports

--- app/index.js
+++ app/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 var dataFile = require('./data/data.json');
 
-app.get('/', function(req,res) {
+router.get('/', function(req,res) {
 
   res.send(`
     <h1>Check out all my peeps!</h1>
     `
   );
 });
+
+module.exports = router;
