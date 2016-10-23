ac62dcb25e817fe7342ef5c5e557937c57dad32a
aleeex <akang2@gmail.com>
2016-10-22 19:19:43 -0700

    moved comment for pathing to images

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,10 @@ router.get('/', function(req,res) {
 
   res.send(`
     <h1>Check out all my peeps!</h1>
-    //remember, the 'public' folder is the root
     <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
     `
+    //remember, the 'public' folder is the root, so the above path to the background
+    //image starts at /images
   );
 });
 
