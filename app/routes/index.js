16643848344b31f7af5e1a913e495455abd51149
aleeex <akang2@gmail.com>
2016-10-23 05:46:04 -0700

    added link to css

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get('/', function(req,res) {
 
   res.send(`
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <h1>Check out all my peeps!</h1>
     <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
     `
